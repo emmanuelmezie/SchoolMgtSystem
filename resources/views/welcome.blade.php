dd2640b7f995a7ec471b397c1b8a1c61a0b58286
victorodoemelam <vicodoemelam@gmail.com>
2017-11-30 13:22:48 +0100

    i changed the index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    School Mgt System
                 </div>
 
                 <div class="links">
